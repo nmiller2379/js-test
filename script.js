615586ecc7b389f8282773f7b36ff238efda8867
Miller <nmiller2379@gmail.com>
2020-10-16 23:24:02 -0600

    created inititial HTM, CS and JS documents

---
+++ script.js
@@ -0,0 +1,94 @@
+// Create a timed JS multiple-choice test
+
+// DECLARE VARIABLES needed for game
+var testContainer = document.getElementById("test")
+var startButton = document.getElementById("start")
+var resultsContainer = document.getElementById("results")
+var testQuestions = [
+    {
+        question: "What is Javascript?",
+        answers: {
+            a: "A fancy new coffee drink at Starbucks.",
+            b: "One of the three main programming language that underpin the web.",
+            c: "None of the above."
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the primary function of Javascript?",
+        answers: {
+            a: "It allows the creation of complex, interactive features on web pages.",
+            b: "To make begginner coding students cry.",
+            c: "It allows us to write about our love of coffee."
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which of the following is a data type in Javascript?",
+        answers: {
+            a: "Bilingual type.",
+            b: "String type.",
+            c: "Dark roast." 
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What are variables in Javascript?",
+        answers: {
+            a: "The temperature of your coffee at Starbucks.",
+            b: "Methods for executing an operation.",
+            c: "Containers for storing information."
+        },
+        correctAnswer: "c"
+
+    },
+    {
+        question: "Do you need a cup of coffee after all these questions?",
+        answers: {
+            a: "Yes.",
+            b: "No.",
+            c: "There are too many coffee puns in this test."
+        },
+        correctAnswer: "a"
+    }
+]
+console.log(testQuestions)
+
+// CREATE functions to build the test and to display the results
+function createTest() {
+    
+}
+function displayResults() {
+    
+}
+
+// Start button
+
+
+            //Click event listener that will return the first question of the game and start the timer.
+
+    // Starts a timer
+        //Timer code from Class 4.3 on Oct 10
+    
+    // Presents user with a question
+
+
+
+// Create arrays needed for game
+
+    // Look back through class activities to find a game
+
+// After the first question is anwered a second question is returned.
+
+// When a question is answered incorrectly, time is deducted from the timer.
+
+// The game is over when all the questions are answered or the timer reaches 0
+
+    //When the game is over a message telling the user the game has concluded is returned.
+
+        // A button the user can click to take them to a field to enter their initials next to their score is returned.
+
+        // Another button give the user the opportunity to start over
+
+    //The score and initials are saved in user memory and retained.
+
